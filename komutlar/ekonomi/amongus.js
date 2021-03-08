e65dc7e64501a239ecb02f389ebca71e8d8d5161
ismailaydinlik <78856130+ismailaydinlik@users.noreply.github.com>
2021-03-08 11:07:25 +0300

    Update amongus.js

--- komutlar/ekonomi/amongus.js
+++ komutlar/ekonomi/amongus.js
@@ -1,5 +1,5 @@
 module.exports = {
-    name: "tld",
+    name: "amongus",
     code: `$setUserVar[para;$sub[$getUserVar[para];200000]]
 $dm $color[DARKBLUE]$description[Dostum oyunun [burada\\](https://drive.google.com/file/d/1jffb4OnG2ZqSaMh6lCqlQRUpKhpZ-bZV/view)!] $thumbnail[$authorAvatar]
     $onlyIf[$getUserVar[para]>$sum[200000;1];]`
