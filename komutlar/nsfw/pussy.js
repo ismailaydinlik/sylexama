e0b059b571883dc03398dee1e25660a77f2896df
ismailaydinlik <78856130+ismailaydinlik@users.noreply.github.com>
2021-03-12 10:30:41 +0300

    Delete komutlar/nsfw directory

--- komutlar/nsfw/pussy.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    name: "pussy",
-    code: `$color[DARKBLUE]$description[**Pussy**]$footer[$username;$addTimestamp]$image[$jsonRequest[https://api.avux.ga/pussy;text]]
-    $onlyNSFW[]`
-}
