9d28e40f61e90033ad7a27d3ba616a7963a5feb2
ismailaydinlik <78856130+ismailaydinlik@users.noreply.github.com>
2021-03-12 11:46:02 +0300

    Update fakeyorum.js

--- komutlar/eglence/fakeyorum.js
+++ komutlar/eglence/fakeyorum.js
@@ -2,5 +2,6 @@ module.exports = {
     name: "fakeyorum",
     code: `$attachment[https://some-random-api.ml/canvas/youtube-comment?avatar=$replaceText[$authorAvatar;webp;png]&comment=$message&username=$username;yorum.png]
     $onlyIf[$message!=;]
+$suppressErrors[]
     `
 }
