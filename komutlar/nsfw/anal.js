eb0aa571cfd120eb37e53a487815e00b6cf25f91
ismailaydinlik <78856130+ismailaydinlik@users.noreply.github.com>
2021-03-12 10:35:03 +0300

    Add files via upload

---
+++ komutlar/nsfw/anal.js
@@ -0,0 +1,5 @@
+module.exports = {
+    name: "anal",
+    code: `$color[DARKBLUE]$description[**Anal**]$footer[$username;$addTimestamp]$image[$jsonRequest[https://api.avux.ga/anal;text]]
+    $onlyNSFW[]`
+}
