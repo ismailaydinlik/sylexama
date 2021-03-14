0e608c95336433f384f96575d173507984dfd218
ismailaydinlik <78856130+ismailaydinlik@users.noreply.github.com>
2021-03-14 13:05:23 +0300

    Update hoşgeldinkanal.js

--- komutlar/mod/hoşgeldinkanal.js
+++ komutlar/mod/hoşgeldinkanal.js
@@ -3,5 +3,7 @@ module.exports = {
     code: `$reply[$messageID;**Sunucunuzun hoşgeldin kanalı <#$mentionedChannels[1]> olarak ayarlandı.**]
     $setServerVar[hoşgeldinkanal;$mentionedChannels[1]]
     $onlyIf[$mentionedChannels[1]!=;]
-    $suppressErrors`
+    $onlyPerms[managechannels;]
+    $suppressErrors[]
+`
 }
