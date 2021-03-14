8b1315f57e1eef48884a8ca14d6f5b237f05c3b9
ismailaydinlik <78856130+ismailaydinlik@users.noreply.github.com>
2021-03-14 16:29:01 +0300

    Update bot.js

--- komutlar/genel/bot.js
+++ komutlar/genel/bot.js
@@ -4,21 +4,22 @@ module.exports = {
     $thumbnail[$authorAvatar]
     $author[$username[$clientid];$useravatar[$clientid]]
     $color[DARKBLUE]
-    $addField[Diğer;
-    • Toplam Komut: $commandsCount
-    • Gecikme: $botPing ms
-    • Uptime: $uptime
-    • Sahip: $usertag[$botownerid]]
-    $addField[Versiyonlar;
-    • NodeJS Versiyonu: $getObjectProperty[nodev]
-    • Discord.js Versiyonu: $getObjectProperty[discordv]
-    • DBD.js versiyonu: v2.1.1
-    ]
-    $addField[Hosting;
-    • CPU Kullanımı: $cpu%
-    • CPU Modeli: $djsEval[require ('os').cpus()[0\\].model;yes]
-    • CPU Platformu: $djsEval[require ('os').platform();yes]
-    • Bellek Kullanımı: $roundtenth[$djsEval[process.memoryUsage().rss / 1024 / 1024;yes];2] MB]
+    $addField[Diğer;\`\`\`
+• Toplam Komut: $commandsCount
+• Gecikme: $botPing ms
+• Uptime: $uptime
+• Toplam Sunucu: $serverCount
+• Toplam Kullanıcı: $allMembersCount\`\`\`]
+    $addField[Versiyonlar;\`\`\`
+• NodeJS Versiyonu: $getObjectProperty[nodev]
+• Discord.js Versiyonu: $getObjectProperty[discordv]
+• DBD.js versiyonu: v2.1.1
+    \`\`\`]
+    $addField[Hosting;\`\`\`
+• CPU Kullanımı: $cpu%
+• CPU Modeli: $djsEval[require ('os').cpus()[0\\].model;yes]
+• CPU Platformu: $djsEval[require ('os').platform();yes]
+• Bellek Kullanımı: $ramMB\`\`\`]
      $djseval[d.object.nodev = process.version
     d.object.discordv = require('discord.js').version
     $createObject[{}]]`
