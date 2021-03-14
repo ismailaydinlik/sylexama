2fef33121216a5a9e0309da063860181375b6012
ismailaydinlik <78856130+ismailaydinlik@users.noreply.github.com>
2021-03-14 13:36:54 +0300

    Delete hoşgeldinkanal.js

--- komutlar/mod/hoşgeldinkanal.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: "hoşgeldinkanal",
-    code: `$reply[$messageID;**Sunucunuzun hoşgeldin kanalı <#$mentionedChannels[1]> olarak ayarlandı.**]
-    $setServerVar[hoşgeldinkanal;$mentionedChannels[1]]
-    $onlyIf[$mentionedChannels[1]!=;]
-    $onlyPerms[managechannels;]
-`
-}
